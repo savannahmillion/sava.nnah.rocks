4db7087f0a8f8eaf7169e78982ec7b37f95d6ee6
Savannah <savannahmillion@gmail.com>
2015-01-06 14:17:49 -0600

    fixed footer, separated js

--- _themes/self/js/self.js
+++ _themes/self/js/self.js
@@ -6,3 +6,44 @@ $.jribbble.getShotsByPlayerId('savannahmillion', function (work) {
     $('#work').append('<li><a href="' + shot.url + '"><img src="' + shot.image_url + '" alt="' + shot.title + '"></a><a href="' + shot.url + '"><p>' + shot.title + ', from Dribbble &rarr;</p></a></li> ');
     });
  }, {page: 1, per_page: 1});
+
+function getDate(){
+  var today = new Date();
+  var year = today.getFullYear();
+  document.getElementById("currentDate").innerHTML = year;
+}
+
+var _gaq = _gaq || [];
+_gaq.push(['_setAccount', 'UA-36984614-1']);
+_gaq.push(['_trackPageview']);
+
+(function() {
+var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+})();
+
+
+$(document).ready(function() {
+	// grab the initial top offset of the navigation 
+   	var stickyNavTop = $('.nav').offset().top;
+   	
+   	// our function that decides weather the navigation bar should have "fixed" css position or not.
+   	var stickyNav = function(){
+	    var scrollTop = $(window).scrollTop(); // our current vertical position from the top
+	         
+	    // if we've scrolled more than the navigation, change its position to fixed to stick to top,
+	    // otherwise change it back to relative
+	    if (scrollTop > stickyNavTop) { 
+	        $('.nav').addClass('sticky');
+	    } else {
+	        $('.nav').removeClass('sticky'); 
+	    }
+	};
+
+	stickyNav();
+	// and run it again every time you scroll
+	$(window).scroll(function() {
+		stickyNav();
+	});
+});
